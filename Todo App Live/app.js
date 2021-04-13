46dacc13abc6cfe9702d0bce34d220e94f584f0d
MaviaAli <maviaali597@gmail.com>
2021-04-14 03:51:11 +0500

    realtime todoapp updated

--- Todo App Live/app.js
+++ Todo App Live/app.js
@@ -8,16 +8,15 @@ var month = months[d.getMonth()];
 var year = d.getFullYear();
 var day = days[d.getDay()];
 
-var todayDate = document.getElementById("date");
-var todayMonth = document.getElementById("month");
-var todayYear = document.getElementById("year");
+var todayDate = document.getElementById("todayDate");
+var todayMonth = document.getElementById("todayMonth");
+var todayYear = document.getElementById("todayYear");
 var dateText = document.createTextNode(date);
 var monthText = document.createTextNode(month);
 var yearText = document.createTextNode(year);
 todayDate.appendChild(dateText);
 todayMonth.appendChild(monthText);
 todayYear.appendChild(yearText);
-
 var todayDay = document.getElementById("day");
 var dayText = document.createTextNode(day);
 todayDay.appendChild(dayText);
@@ -36,24 +35,27 @@ else {
 
 // Code of Add Todo Button
 
-function addTodo() {
-    var input = document.getElementById("todo");
+var input = document.getElementById("todo");
 
+function addTodo() {
     if(input.value == "") {
         alert("Can't be empty!\nPlease enter something");
     }
-
     else {
         var key = firebase.database().ref('todoList').push().key;
         var data = {
         item: input.value,
         key: key
         }
-
         firebase.database().ref('todoList/' + key).set(data);
-
         return data;
     }    
+}
+
+input.onkeypress = function() {
+    if(event.keyCode == 13) {
+        addTodo();
+    }
 }
 
 // Code of getting data from FireBase to display in Todo App 
@@ -61,30 +63,16 @@ function addTodo() {
 function getData() {
     firebase.database().ref('todoList').on('child_added',function(data){
         var input = document.getElementById("todo");
-        var list = document.getElementById("list");
-        var key = document.createElement("p");
-        var keyText = document.createTextNode(data.val().key);
-        key.appendChild(keyText);
-        var elem = document.createElement("input");
-        elem.setAttribute("id","task");
-        elem.setAttribute("spellcheck","false");
-        elem.disabled = true;
-        elem.value = data.val().item;
-        list.appendChild(elem);
-        list.appendChild(key);
-        key.hidden = true;
-        var dltBtn = document.createElement("i");
-        var editBtn = document.createElement("i");
-        dltBtn.setAttribute("id","dltBtn");
-        editBtn.setAttribute("id","editBtn");
-        var i = data.val().item;
-        dltBtn.setAttribute("onclick","dltTask(this)");
-        editBtn.setAttribute("onclick","editTask(this)");
-        dltBtn.setAttribute("class","fa fa-trash");
-        editBtn.setAttribute("class","fa fa-pencil");
-        list.appendChild(dltBtn);
-        list.appendChild(editBtn);
+        var taskList = document.getElementById("taskList");
         input.value = "";
+        taskList.innerHTML += `<li>
+        <div>
+            <input type="text" id="task" value='${data.val().item}' spellcheck="false" disabled>
+        </div><div>
+            <i class="fa fa-pencil" id="editBtn" onclick="editTask('${data.val().key}',this)"></i>
+            <i class="fa fa-minus" id="deleteBtn" onclick="deleteTask('${data.val().key}',this)"></i>
+        </div>
+    </li> `        
     });
 }
 
@@ -92,50 +80,49 @@ getData();
 
 // Code of Delete Todo Item Button
 
-function dltTask(x) {
-    firebase.database().ref('todoList').child(x.previousSibling.innerHTML).remove();
-    x.previousSibling.previousSibling.remove();
-    x.previousSibling.remove();
-    x.nextSibling.remove();
-    x.remove();
+function deleteTask(key,element) {
+    firebase.database().ref('todoList').child(key).remove();
+    element.parentElement.parentElement.remove();
 }
 
 // Code of Edit Todo Item Button
 
-function editTask(y) {
-    if(y.getAttribute("class") == "fa fa-pencil") {
-        pencil(y)
+function editTask(key,element) {
+    if(element.getAttribute("class") == "fa fa-pencil") {
+        pencil(element);
     }
-    else if(y.getAttribute("class") == "fa fa-check") {
-        check(y)
+    else if(element.getAttribute("class") == "fa fa-check") {
+        check(key,element);
     }
 }
 
-function pencil(y) {
-    var innerInp = y.previousSibling.previousSibling.previousSibling;
+
+function pencil(element) {
+    var innerInp = element.parentElement.previousSibling.childNodes[1];
     innerInp.disabled = false;
     innerInp.focus();
-    y.removeAttribute("class","fa fa-pencil");
-    y.setAttribute("class","fa fa-check");
-    y.style.backgroundColor = "dodgerblue";
-    innerInp.style.borderBottom = "2px solid slateblue";
+    innerInp.selectionStart = innerInp.selectionEnd = innerInp.value.length;
+    innerInp.setAttribute('class', 'edit');
+    element.setAttribute("class","fa fa-check");
+    element.style.backgroundColor = "royalblue";
+    element.style.padding = '6px';
 }
 
-function check(y) {
-    var innerInp = y.previousSibling.previousSibling.previousSibling;
+function check(key,element) {
+    var innerInp = element.parentElement.previousSibling.childNodes[1];
     if(innerInp.value == "") {
         alert("Can't be empty!\nPlease enter something");    
     }
     else {
-        firebase.database().ref('todoList').child(y.previousSibling.previousSibling.innerHTML).set({
+        firebase.database().ref('todoList').child(key).set({
             item: innerInp.value,
-            key: y.previousSibling.previousSibling.innerHTML
+            key: key
         });
         innerInp.disabled = true;
-        y.removeAttribute("class","fa fa-check");
-        y.setAttribute("class","fa fa-pencil");
-        y.style.backgroundColor = "green";
-        innerInp.style.borderBottom = "2px solid dodgerblue";
+        innerInp.removeAttribute('class');
+        element.setAttribute("class","fa fa-pencil");
+        element.style.backgroundColor = "seagreen";
+        element.style.padding = '6px 8px';
     }   
 }
 
@@ -143,6 +130,6 @@ function check(y) {
 
 function deleteAll() {
     firebase.database().ref('todoList').remove();
-    var list = document.getElementById("list");
-    list.innerHTML = "";
+    var taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
 }
